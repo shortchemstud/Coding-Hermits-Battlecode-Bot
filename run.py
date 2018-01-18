87a414a8bbce3c386a7bb8ae8a0333d649c7eeab
shortchemstud <jhc1000000@aol.com>
2018-01-18 07:38:12 -0500

    New Update
    UPDATED STUFF

--- run.py
+++ run.py
@@ -46,6 +46,7 @@ def fuzzygoto(unit,dest):
 
 gc.queue_research(bc.UnitType.Rocket)
 gc.queue_research(bc.UnitType.Ranger)
+gc.queue_research(bc.UnitType.Healer)
 
 while True:
 	try:
@@ -57,6 +58,7 @@ while True:
 		numKnight = 0
 		numMage = 0
 		numRocket = 0
+		numHealer = 0
 		blueprintLocation = None
 		blueprintWaiting = False
 		rocketLocation = None
@@ -89,6 +91,8 @@ while True:
 				numKnight+=1
 			if unit.unit_type == bc.UnitType.Mage:
 				numMage+=1
+			if unit.unit_type == bc.UnitType.Healer:
+				numHealer+=1
 		print("Number of Workers: " + str(numWorkers))
 		print("Number of Factories: " + str(numFactory))
 		print("Number of Blueprints: " + str(numBlueprint))
@@ -96,18 +100,21 @@ while True:
 		print("Number of Knights: " + str(numKnight))
 		print("Number of Mages: " + str(numMage))
 		print("Number of Rockets: " + str(numRocket))
+		print("Number of Healer" + str(numHealer))
 
 		for unit in gc.my_units():
 			d = random.choice(directions)
-			if unit.unit_type == bc.UnitType.Worker:
+			if unit.unit_type == bc.UnitType.Worker: # Worker micro
+				if gc.karbonite_at(unit.location.map_location()) and gc.can_harvest(unit.id, d):
+					gc.harvest(unit.id, d)
 				d = random.choice(directions)
-				if numWorkers<5 and gc.can_replicate(unit.id,d):
+				if numWorkers<5 and gc.can_replicate(unit.id, d):
 					gc.replicate(unit.id,d)
 					continue
 				if gc.round() > 150 and gc.can_blueprint(unit.id, bc.UnitType.Rocket, d) and gc.karbonite() > bc.UnitType.Rocket.blueprint_cost():
 					gc.blueprint(unit.id, bc.UnitType.Rocket, d)
 					continue
-				if gc.karbonite() > bc.UnitType.Factory.blueprint_cost() and numFactory < 3:#blueprint
+				if gc.karbonite() > bc.UnitType.Factory.blueprint_cost() and numFactory <= 5:#blueprint
 					if gc.can_blueprint(unit.id, bc.UnitType.Factory, d):
 						gc.blueprint(unit.id, bc.UnitType.Factory, d)
 						continue
@@ -127,7 +134,7 @@ while True:
 					gc.move_robot(unit.id, d)
 					continue
 
-			if unit.unit_type == bc.UnitType.Rocket:
+			if unit.unit_type == bc.UnitType.Rocket: # rocket micro
 				passenger = unit.structure_garrison()
 				if rocketWaiting:
 					if len(passenger) < 6 and unit.location.is_on_planet(bc.Planet.Earth):
@@ -150,20 +157,24 @@ while True:
 							gc.unload(unit.id, d)
 							continue
 
-			if unit.unit_type == bc.UnitType.Factory:
+			if unit.unit_type == bc.UnitType.Factory: # Factory micro
 				garrison = unit.structure_garrison()
 				if len(garrison) > 0:#ungarrison
 					if gc.can_unload(unit.id, d):
 						gc.unload(unit.id, d)
+				else:
+					build = random.randint(1,5)
+					if  build == 1 or build == 2 or build == 3 or build == 4:
+						if gc.can_produce_robot(unit.id, bc.UnitType.Ranger) and numRanger <= 75: #produce Rangers
+							gc.produce_robot(unit.id, bc.UnitType.Ranger)
+							continue
+					if build == 5:
+						if gc.can_produce_robot(unit.id, bc.UnitType.Healer) and numHealer <= 20: #produce Healers
+							gc.produce_robot(unit.id, bc.UnitType.Healer)
+							continue
 						continue
-				elif gc.can_produce_robot(unit.id, bc.UnitType.Ranger) and numRanger < 60: #produce Rangers
-					gc.produce_robot(unit.id, bc.UnitType.Ranger)
-					continue
-				elif gc.can_produce_robot(unit.id, bc.UnitType.Knight) and numKnight < 0:#produce Knights
-					gc.produce_robot(unit.id, bc.UnitType.Knight)
-					continue
 
-			if unit.unit_type == bc.UnitType.Knight:
+			if unit.unit_type == bc.UnitType.Knight: # Knight micro
 				if unit.location.is_on_map():#can't move from inside a factory
 					inRangeUnits = gc.sense_nearby_units(unit.location.map_location(), 2)
 					for other in inRangeUnits:
@@ -174,17 +185,46 @@ while True:
 					for other in inViewUnits:
 						if other.team != my_team and gc.is_move_ready(unit.id):
 							el = other.location.map_location()
-							fuzzygoto(unit.id, other.location.map_location())
+							fuzzygoto(unit, el)
 							continue
 					if gc.is_move_ready(unit.id) and gc.can_move(unit.id, d):
 						gc.move_robot(unit.id, d)
 						continue
 
-			if unit.unit_type == bc.UnitType.Ranger:
+			if unit.unit_type == bc.UnitType.Healer: #Healer micro
+				if unit.location.is_on_map():
+					inHealUnits = gc.sense_nearby_units(unit.location.map_location(), 30)
+					for other in inHealUnits:
+						if other.team == my_team and gc.is_heal_ready(unit.id) and gc.can_heal(unit.id, other.id) and other.unit_type == bc.UnitType.Ranger:
+							if other.health <= 80:
+								gc.heal(unit.id, other.id)
+								continue
+					if gc.is_move_ready(unit.id) and gc.can_move(unit.id, d):
+						gc.move_robot(unit.id, d)
+						continue
+
+			if unit.unit_type == bc.UnitType.Mage: # Mage micro
+				if unit.location.is_on_map():
+					inFireballUnits = gc.sense_nearby_units(unit.location.map_location(), 30)
+					for other in inFireballUnits:
+						if other.team != my_team and gc.is_attack_ready(unit.id) and gc.can_attack(unit.id, other.id):
+							gc.attack(unit.id, other.id)
+							continue
+						if other.team != my_team and gc.is_move_ready(unit.id):
+							el = other.location.map_location()
+							fuzzygoto(unit, el)
+							continue
+				if gc.is_move_ready(unit.id) and gc.can_move(unit.id, d):
+					gc.move_robot(unit.id, d)
+					continue
+
+			if unit.unit_type == bc.UnitType.Ranger: # Ranger micro
 				if unit.location.is_on_map():
 					inRangeUnits = gc.sense_nearby_units(unit.location.map_location(), 50)
+					attacking = False
 					for other in inRangeUnits:
 						if other.team != my_team and gc.is_attack_ready(unit.id) and gc.can_attack(unit.id, other.id):
+							attacking = True
 							gc.attack(unit.id, other.id)
 							continue
 					if rocketWaiting:
@@ -193,9 +233,11 @@ while True:
 							rdist = ml.distance_squared_to(rocketLocation)
 							if rdist>2:
 								fuzzygoto(unit, rocketLocation)
-					if gc.is_move_ready(unit.id) and gc.can_move(unit.id, d):
-						gc.move_robot(unit.id, d)
-						continue
+								continue
+					if not attacking:
+						if gc.is_move_ready(unit.id) and gc.can_move(unit.id, d):
+							gc.move_robot(unit.id, d)
+							continue
 
 	except Exception as e:
 		print('Error:', e)
